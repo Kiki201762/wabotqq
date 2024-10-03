58c44c42176f526b529a3f9fbf2a60a038f5dc34
Kiki201762 <renatadhea467@gmail.com>
2024-10-03 19:03:47 +0700

    Add files via upload

---
+++ App/Templates/sewa.js
@@ -0,0 +1,12 @@
+const listsewa_ = `¥ Price Sewa Bot Autoresbot €
+
+🔏  1 Bulan  = 15k
+🔏 2 Bulan   = 20k
+🔏 3 Bulan  = 25k
+🔏 Permanen  = 30k
+
+Untuk Melanjutkan Sewa Silahkan Hubungi Owner`;
+
+module.exports = {
+    listsewa_
+};
